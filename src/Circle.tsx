c967e8dd4a811144984d363d45853f463c4d3e48
jayhug2 <zero900r@gmail.com>
2022-03-12 01:00:10 +0900

    react masterclass start

---
+++ src/Circle.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+interface ContainerProps {
+    bgColor: string;
+    borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+    width:100px;
+    height:100px;
+    background-color: ${props => props.bgColor};
+    border: 3px solid ${props => props.borderColor}
+`;
+
+
+interface CircleProps{
+    bgColor: string;
+    borderColor?: string;
+}
+
+function Circle({bgColor,borderColor}:CircleProps) {
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}/>
+    )
+}
+
+export default Circle;
