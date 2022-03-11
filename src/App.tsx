616b2987b38db7e6ec0fc2c8d6a8f52ff9c3c006
jayhug2 <zero900r@gmail.com>
2022-03-12 02:32:10 +0900

    ts and form study

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value}
+    } = event;
+    setValue(value);
+  }
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello",value);
+  }
+
+
   return (
     <div>
-      <Circle bgColor="cornflowerblue" borderColor="black"/> 
-      <Circle bgColor="teal"/> 
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          placeholder="username" 
+          onChange={ onChange }
+          value={value}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
